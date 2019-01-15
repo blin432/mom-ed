ad180023b01966caa6f499ee26f0e523a55dc4cb
benjamin lin <blin432@gmail.com>
2019-01-14 22:10:15 -0500

    finished registering new users to databse, set up express to use passport and session

--- public/loginPage/main.js
+++ public/loginPage/main.js
@@ -6,9 +6,11 @@ var createAccountButton = document.getElementById("create-account-button");
 var authBox = document.getElementById("auth-box");
 var existingMember = document.getElementById('existing-member');
 var signUpBox = document.getElementById("sign-up");
-var userName = document.getElementById("sign-up-username").value;
-var userEmail = document.getElementById("sign-up-email").value;
-var userPassword = document.getElementById("sign-up-password").value;
+var userName = document.getElementById("sign-up-username");
+var userEmail = document.getElementById("sign-up-email");
+var userPassword = document.getElementById("sign-up-password");
+var loginEmail = document.getElementById("login-email");
+var loginPw = document.getElementById("login-password");
 
 
 function showLoginForm(){
@@ -36,14 +38,26 @@ function cancel(){
 
 function createNewAccount(){
 
-    axios.post('/auth/register', { email: userEmail, password: userPassword }).then(function(response) {
+    axios.post('/auth/register', { username: userName.value, email: userEmail.value, password: userPassword.value }).then(function(response) {
+      
         var user = response.data;
-        consolg.log(user);
-        alert(`Account created for ${user.email}`);
+        
+        alert(`Account created for ${user}`);
     }).catch(function(){
-        alert(`That user is already registered`);
+        // alert(`That user is already registered`);
     });
 
 
+}
+
+function loginToExistingAccount(){
+    axios.post('/auth/login',{email:loginEmail.value,password:loginPw.value}).then(function(response){
+        console.log(response);
+        alert('succes');
+    }).catch(function(err){
+        console.log(err);
+    });
+
+    
 }
 
--- server.js
+++ server.js
@@ -1,33 +1,119 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-// const bcrypt = require('bcryptjs');
-// const passport =require('passport');
-// const LocalStrategy = require('passport-local').Strategy;
-// const session = require('session');
+const bcrypt = require('bcryptjs');
+const passport =require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const session = require('express-session');
 
+const db= require('./models'); 
 var app = express();
 
 app.use (bodyParser.json());
-app.use (bodyParser.urlencoded({extended:false}));
+app.use (bodyParser.urlencoded({extended:true}));
+app.use(session({secret:'password'}));
 
-app.set('view engine', 'ejs');
-app.set ('views','app/views'); //need to set when path is defined
-const db= require('/models');  //need to set path
-
-app.post('/api/username/:addUsername',function(req,res,next){
-    console.log(db.users)
-    if(!user){
-        db.users.create({userName:'Eric',email:'blin432@gmail.com',password:'asdfasf'})
-        .then(function(user){
-            console.log(user);
-        });
-    } else{
-        res.send(err);
-    }; 
-});
-
+// app.set('view engine', 'ejs'); //set when ejs template is done
+// app.set ('views','app/views'); //need to set when path is defined
  
 
+
+passport.use(new LocalStrategy({usernameField:'email'},function(email,password,done){
+    console.log('hi');
+    db.users.findAll({where:{email:email}}) //finding artist ID to print to console
+    
+                    .then(function(results){
+                        var fetchedPw= results[0].dataValues.password;//console log results to see how to get password
+                        console.log(results[0].dataValues.password);
+                        var isPwMatch = bcrypt.compareSync(password,fetchedPw)//takes hash and compares it to password
+                        if (isPwMatch){
+                            console.log('match');
+                            done(null,results)
+                        }else{
+                            console.log("did not");
+                            done(null,false);
+                        }
+          
+                    }).catch(function(err){
+                        console.log(err);
+                        done(null, false)//passing in false means "note a succsefull login"
+                    });  
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.serializeUser(function(user,done){
+    
+    done(null,{
+        id: user.id,
+        email:user.email
+    });
+});
+passport.deserializeUser(function(cookie,done){
+    db.users.findAll({where:{id:cookie.id}})
+        .then(function(user){
+            console.log(user);
+            done(null,user)
+        });
+});
+
+
+app.post('/auth/register',function(req,res,next){ //registers the user to database
+    console.log(req.body);
+        // req.isAuthenticated();  uses cookie and session to see if logged in or authenticated see function isAuthenticated
+        var hashedPw=bcrypt.hashSync(req.body.password,10);
+        db.users.create({username:req.body.username,email:req.body.email,password:hashedPw})
+        .then(function(user){
+            console.log(user);
+            res.json({status:'ok'});
+        }).catch(function(err){
+            console.log(err);
+        });
+    
+});
+// passport.authenticate('local'); endpoint will only run if logged in
+app.post('/auth/login',passport.authenticate('local'),function(req,res,next){
+    res.json({status:'okay'});
+});
+
+
+// to check if people are logging in and show email on screen
+// if not logged in will respond with null
+// app.get('/auth/user',function(req,res,next){
+//     res.json(req.user.email);
+// });
+
+function isAuthenticated(req,res,next){
+    if(req.isAuthenticated()){
+        next();
+    }else{
+        res.json({status:"no"})
+    }
+}
+
+
+app.get('/auth/logout',function(req,res,next){
+    req.logout();
+})
+
+
+ 
+app.put('/schedule/edit',function(req,res,next){
+    console.log(req.body);
+    users.update(
+        {username:req.body.username},
+        {where:req.params.edit}
+    )
+    .then(function(rowsUpdate){
+        res.json(rowsUpdate)
+    })
+    .catch(next);
+
+})
+
+
+
+app.use(express.static('public'));
 var PORT = process.env.PORT || 3000;
 db.sequelize.sync().then(function(){
     app.listen(PORT,function(){
@@ -36,4 +122,3 @@ db.sequelize.sync().then(function(){
 })
 
 
-app.use(express.static('public'));
